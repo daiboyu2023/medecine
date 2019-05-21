c04a13406aafd979f10d6eb4f3aced9c22518d35
daiboyu <195378992@qq.com>
2019-05-21 16:40:32 +0800

    上传项目

---
+++ src/main/java/com/medecine/MedecineApplication.java
@@ -0,0 +1,13 @@
+package com.medecine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedecineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedecineApplication.class, args);
+	}
+
+}
